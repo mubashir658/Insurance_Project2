f54a4e09ad18661c0a9f810940e015e19bdc6def
mubashir658 <mohammedmubashirali658@gmail.com>
2026-05-13 08:58:43 +0530

    added dynamic ports

--- dl/app.py
+++ dl/app.py
@@ -278,8 +278,12 @@ def predict():
 
 if __name__ == '__main__':
 
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5001))
+
     app.run(
-        debug=True,
         host='0.0.0.0',
-        port=5001
+        port=port
     )
